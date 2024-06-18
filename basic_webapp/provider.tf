948855be64aec795daff42b9fe6b01c705e131b5
APtheepan <APtheepan@example.com>
2024-06-17 22:52:32 -0400

    modified:   basic_webapp/provider.tf

--- basic_webapp/provider.tf
+++ basic_webapp/provider.tf
@@ -9,6 +9,6 @@ terraform {
 provider "aws" {
   # Configuration options
   region                   = "us-east-1"
-  #shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "vscode"
+ # shared_credentials_files = ["~/.aws/credentials"]
+ # profile                  = "vscode"
 }
